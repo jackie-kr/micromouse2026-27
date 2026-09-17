79b4abc6436db6f39a4679425bef08c2f9a5a920
biasTheThird <littleclover7@outlook.com>
2026-09-17 14:22:26 -0400

    I can't count I'm legally blind
    changed the dimensions from 15x15 to 16x16 cells 💀

--- arrayTranslator.h
+++ arrayTranslator.h
@@ -2,9 +2,9 @@
 
 class arrayTranslator {
     private:
-    std::array<std::array<int, 15>, 16> horz;
-    std::array<std::array<int, 16>, 15> vert;
-    std::array<std::array<char,61>, 31> maze;
+    std::array<std::array<int, 16>, 17> horz;
+    std::array<std::array<int, 17>, 16> vert;
+    std::array<std::array<char,65>, 33> maze;
 
 
 
@@ -12,14 +12,14 @@ class arrayTranslator {
     
 
 
-    arrayTranslator(const std::array<std::array<int, 15>, 16>&h, const std::array<std::array<int, 16>, 15>&v) : horz(h), vert(v) {
+    arrayTranslator(const std::array<std::array<int, 16>, 17>&h, const std::array<std::array<int, 17>, 16>&v) : horz(h), vert(v) {
         
     }
 
-    std::array<std::array<char,61>, 31> translate() {
+    std::array<std::array<char,65>, 33> translate() {
 
-        for (int i = 0; i < 31; i++) {
-            for (int j = 0; j < 61; j++) {
+        for (int i = 0; i < 33; i++) {
+            for (int j = 0; j < 65; j++) {
                 
                 maze[i][j] = ' ';
                 
@@ -28,8 +28,8 @@ class arrayTranslator {
         
         
         //This fills in the nodes.
-        for (int a = 0; a < 31; a += 2) {
-            for (int b = 0; b < 61; b++) {
+        for (int a = 0; a < 33; a += 2) {
+            for (int b = 0; b < 65; b++) {
                 if (((b+1)%4 ==1)) {
                     maze[a][b] = '+';
                 }
@@ -40,8 +40,8 @@ class arrayTranslator {
         //filling in the rows:
 
         
-        for (int hy=0;hy<15;hy++) {
-            for (int hx=0;hx<16;hx++) {
+        for (int hy=0;hy<16;hy++) {
+            for (int hx=0;hx<17;hx++) {
                 if(horz[hx][hy]==0) {
                     maze[hx*2][hy*4 + 1] = ' ';
                     maze[hx*2][hy*4 + 2] = ' ';
@@ -56,8 +56,8 @@ class arrayTranslator {
         }
 
         //filling in columns
-        for (int cy=0;cy<16;cy++) {
-            for (int cx=0;cx<15;cx++) {
+        for (int cy=0;cy<17;cy++) {
+            for (int cx=0;cx<16;cx++) {
                 if(vert[cx][cy]==0) {
                     maze[(cx+1)*2-1][cy*4] = ' ';
                 }
